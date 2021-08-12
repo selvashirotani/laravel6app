de54af15bcd1f6262e035cc6772043e295fbe095
城谷知伽 <shirotaniharuka@jouyachitoginoMacBook-ea.local>
2021-08-12 20:55:14 +0900

    課題3カテゴリ選択完了

--- resources/views/item/item.blade.php
+++ resources/views/item/item.blade.php
@@ -45,7 +45,7 @@
                         <select name = "product_category_id" id="category">
                         <!-- onchange=”createCategory(this.value)” 選択をする度にJavaScriptのcreateCategoryという関数を呼び出し -->
                         <!-- また関数を呼び出す際に選択した値を「this.value」にて渡しています -->
-                            <option value="">選択してください</option>
+                            <option value="" >選択してください</option>
                             @foreach ($category as $index => $name)
                             @if((!empty($request->product_category_id) && $request->product_category_id == $index) || old('product_category_id') == $index )
                             <option value="{{$index}}" selected>{{$name}}</option>
@@ -55,8 +55,8 @@
                             @endforeach
                         </select>
                         
-                        <select name = "product_subcategory_id" id="subcategory" disabled>
-                            <option value="">選択してください</option>
+                        <select name = "product_subcategory_id" id="subcategory">
+                            <option id="not_select" value="">選択してください</option>
                             @foreach ($subcategory as $index => $name)
                             @if((!empty($request->product_subcategory_id) && $request->product_subcategory_id == $index) || old('product_subcategory_id') == $index )
                             <option value="{{$index}}" selected>{{$name}}</option>
@@ -69,11 +69,25 @@
                         <script >
                             // セレクトボックスの連動
                             // 親カテゴリのselect要素が変更になるとイベントが発生
-                            jQuery(function(){
+                            var value_sub = $('#subcategory').val();
+                            var value = $('#category').val();
+                            //サブカテゴリに値入ってなかったらサブカテゴリは見えなくする。
+                            if(value_sub==""){
+                                $('#subcategory > option').attr('style', 'display: none;');
+                                jQuery(function(){
                                 function some_handler(){
                                     //実行したい内容をここに書く
-                                    var cate_val = $(this).val();
                                     subcategory.disabled = false;
+                                    var cate_val = $(this).val();
+                                    var select = document.getElementById('subcategory');
+                                    var options = select.options;
+                                    options[0].selected =true;
+
+                                    // すべて非表示にする
+                                    $('#subcategory > option').attr('style', 'display: none;');
+                                    //選択してくださいを表示
+                                    $('#subcategory > #not_select').attr('selected',true).removeAttr('style');
+
                                     $.ajax({
                                     headers: {
                                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -85,12 +99,14 @@
                                     },
                                     datatype: 'json',
                                     })
-                                    .done(function(data) {  
-                                            // 有効の場合
-                                            $.each(data, function(key, value) {  
-                                                $('#subcategory option[value="'+value.id+'"]').toggle();
-                                            })                                       
-                                        
+                                    .done(function(data) {
+
+                                        // 有効の場合
+                                        $.each(data, function(key, value) {  
+                                            // 紐づいている子カテゴリのIDなので非表示させる
+                                            $('#subcategory > option[value="'+value.id+'"]').removeAttr('style');
+                                        })                          
+                                    
                                     })
                                     .fail(function() {
                                     console.log('失敗');
@@ -101,13 +117,89 @@
                                 $("#category").change(some_handler);
                                 $("#category").focus(some_handler);
                             });
+                            }else{ //サブカテゴリに値入ってるとき
+                                //サブカテゴリが変更されたら
+                                jQuery(function(){
+                                function subchange(){
+                                     //ajax通信
+                                    $.ajax({
+                                    headers: {
+                                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                    },
+                                    url: '/fetch/category',
+                                    type: 'POST',
+                                    data: {
+                                        "product_category_id" : value,
+                                    },
+                                    datatype: 'json',
+                                    })
+                                    .done(function(data) {
+                                        // すべて非表示にする
+                                        $('#subcategory > option').attr('style', 'display: none;');
+                                        //選択してくださいを表示
+                                        $('#subcategory > #not_select').attr('selected',true).removeAttr('style');
 
+                                        // 有効の場合
+                                        $.each(data, function(key, value) {  
+                                        
+                                        // 紐づいている子カテゴリのIDなので非表示させる
+                                        $('#subcategory > option[value="'+value.id+'"]').removeAttr('style');
+                                        })                          
 
-                            $(document).on('change', '#subcategory', function() {
-                                var product_subcategory_id = $(this).val();
-                                console.log(product_sub_category_id);
-                            })
+                                    })
+                                    .fail(function() {
+                                    console.log('失敗');
+                                    }); 
 
+                                 }
+                                    //どちらのイベントでも同じ関数が実行される
+                                    $("#subcategory").change(subchange);
+                                    $("#subcategory").focus(subchange);
+                                });
+
+                            
+                                jQuery(function(){
+                                function nextchange(){
+                                    value = $('#category').val();
+                                    var select = document.getElementById('subcategory');
+                                    var options = select.options;
+                                    options[0].selected =true;
+                                    
+                                    $.ajax({
+                                    headers: {
+                                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                    },
+                                    url: '/fetch/category',
+                                    type: 'POST',
+                                    data: {
+                                        "product_category_id" : value,
+                                    },
+                                    datatype: 'json',
+                                    })
+                                    .done(function(data) {
+                                        // すべて非表示にする
+                                        $('#subcategory > option').attr('style', 'display: none;');
+                                        subcategory.disabled = false;
+                                        //選択してくださいを表示
+                                        $('#subcategory > #not_select').removeAttr('style');
+                                        // 有効の場合
+                                        $.each(data, function(key, value) {  
+                                            // 紐づいている子カテゴリのIDなので非表示させる
+                                            $('#subcategory > option[value="'+value.id+'"]').removeAttr('style');
+                                        })                          
+                                    
+                                    })
+                                    .fail(function() {
+                                    console.log('失敗');
+                                    }); 
+                                }
+
+                                    //どちらのイベントでも同じ関数が実行される
+                                    $("#category").change(nextchange);
+                                    
+                                });
+                            
+                            }
 
                         </script>
 
