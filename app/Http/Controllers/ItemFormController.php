3e867cd614627f1788774e81f826e42ca4aa784c
城谷知伽 <shirotaniharuka@jouyachitoginoMacBook-ea.local>
2021-08-16 18:44:15 +0900

    課題3修正

--- app/Http/Controllers/ItemFormController.php
+++ app/Http/Controllers/ItemFormController.php
@@ -58,7 +58,7 @@ class ItemFormController extends Controller
             ));
         }else{
             return view("item.item",compact(
-                'category','subcategory',
+                'category','subcategory'
             ));
         }
 
